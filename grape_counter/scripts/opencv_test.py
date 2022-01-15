220e3ec2a4638b4452deef0e424172c347c02c84
James Bennett <james@bennetts.me.uk>
2022-01-15 18:15:44 +0000

    capture images for tests

--- grape_counter/scripts/opencv_test.py
+++ grape_counter/scripts/opencv_test.py
@@ -10,13 +10,16 @@ from numpy import mean
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
+import cv2
+import os
+
 
 class image_converter:
 
     def __init__(self):
 
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/thorvald_001/kinect2_front_camera/hd/image_color_rect",
+        self.image_sub = rospy.Subscriber("/thorvald_001/kinect2_right_camera/hd/image_color_rect",
                                           Image, self.image_callback)
         # self.image_sub = rospy.Subscriber(
         #     "/camera/rgb/image_raw",
@@ -27,6 +30,7 @@ class image_converter:
         namedWindow("masked")
         namedWindow("canny")
         cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        im_big = cv_image
         cv_image = resize(cv_image, None, fx=0.2, fy=0.2, interpolation = INTER_CUBIC)
 
         mask = inRange(cv_image, (0, 150, 150), (255, 255, 255))
@@ -34,6 +38,11 @@ class image_converter:
         gray_img = cvtColor(cv_image, COLOR_BGR2GRAY)
         img3 = Canny(gray_img, 10, 200)
         imshow("canny", img3)
+
+        # save image
+        path = os.path.join(os.path.expanduser('~'), 'cmp9767_ws/src/CMP9767/grape_counter/grapes2.png')
+        cv2.imwrite(path, im_big)
+        self.image_sub.unregister()
 
         imshow("Image window", cv_image)
         waitKey(1)
